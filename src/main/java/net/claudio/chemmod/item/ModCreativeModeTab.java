94c0d61d0adc370e5ee3cc49d9ba9d3615fdf334
xenolufarius <xenolufarius@hotmail.com>
2023-10-05 20:44:09 -0500

    Block textures #3

--- src/main/java/net/claudio/chemmod/item/ModCreativeModeTab.java
+++ src/main/java/net/claudio/chemmod/item/ModCreativeModeTab.java
@@ -1,13 +1,13 @@
 package net.claudio.chemmod.item;
 
+import net.claudio.chemmod.block.ModBlocks;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
     public static final CreativeModeTab CHEM_TAB = new CreativeModeTab("chemtab") {
         @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ModItems.CHEMICAL.get());
+        public ItemStack makeIcon() {return new ItemStack(ModItems.HELIUM.get());
         }
     };
     public static final CreativeModeTab SALT_TAB = new CreativeModeTab("salttab") {
@@ -18,8 +18,7 @@ public class ModCreativeModeTab {
     };
     public static final CreativeModeTab BLOCKS_TAB = new CreativeModeTab("blockstab") {
         @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ModItems.ARSENIC.get());
+        public ItemStack makeIcon() {return new ItemStack(ModBlocks.BERYLLIUM_BLOCK.get());
         }
     };
 
